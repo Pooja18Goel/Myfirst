421a30c30e2cff308806bed6ca4688cc67b3b6db
Pooja18Goel <goyalswati.1994@gmail.com>
2019-05-01 12:26:23 +0530

    Myfirst

---
+++ sum.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" enter the number of array");
+		Scanner ob = new Scanner(System.in);
+		int n = ob.nextInt();
+		int i, sum=0;
+		for(i=0; i<6; i++) {
+			
+			
+		}
+	}
+
+}
